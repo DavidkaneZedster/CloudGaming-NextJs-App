c4a0a9c3dd1076068b28c25b7898cd473928818e
David <kdavid161262@yandex.ru>
2022-07-20 18:49:33 +0300

    Initial commit

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <div className="container">
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
